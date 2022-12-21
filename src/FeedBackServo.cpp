bac0b2d5112e1417414b9db3e55d2f3d81bcb7f9
HyodaKazuaki <kazuaki@ex-t.jp>
2022-12-21 14:21:30 +0900

    code formatting

--- src/FeedBackServo.cpp
+++ src/FeedBackServo.cpp
@@ -35,26 +35,30 @@ void FeedBackServo::setServoControl(byte servoPinNumber)
     Parallax.attach(servoPinNumber);
 }
 
-void FeedBackServo::setKp(float _Kp) {
+void FeedBackServo::setKp(float _Kp)
+{
     FeedBackServo::Kp = _Kp;
 }
 
 void FeedBackServo::rotate(int degree, int threshold)
 {
     float output, offset, value;
-    for(int errorAngle = degree - angle; abs(errorAngle) > threshold; errorAngle = degree - angle) {
+    for (int errorAngle = degree - angle;
+         abs(errorAngle) > threshold;
+         errorAngle = degree - angle)
+    {
         output = errorAngle * Kp;
-        if(output > 200.0)
+        if (output > 200.0)
             output = 200.0;
-        if(output < -200.0)
+        if (output < -200.0)
             output = -200.0;
-        if(errorAngle > 0)
+        if (errorAngle > 0)
             offset = 30.0;
-        else if(errorAngle < 0)
+        else if (errorAngle < 0)
             offset = -30.0;
         else
             offset = 0.0;
-        
+
         value = output + offset;
         Parallax.writeMicroseconds(1490 - value);
     }
@@ -68,46 +72,55 @@ int FeedBackServo::Angle()
 
 void FeedBackServo::pinCheck(byte _feedbackPinNumber)
 {
-    // Check pin number
-    #ifdef ARDUINO_AVR_UNO
-    if(_feedbackPinNumber != 2 && _feedbackPinNumber != 3)
+// Check pin number
+#ifdef ARDUINO_AVR_UNO
+    if (_feedbackPinNumber != 2 &&
+        _feedbackPinNumber != 3)
         exit(1);
-    #endif
-    #ifdef ARDUINO_AVR_LEONARDO
-    if(_feedbackPinNumber != 0 && _feedbackPinNumber != 1 && _feedbackPinNumber != 2 && _feedbackPinNumber != 3 && _feedbackPinNumber != 7)
+#endif
+#ifdef ARDUINO_AVR_LEONARDO
+    if (_feedbackPinNumber != 0 &&
+        _feedbackPinNumber != 1 &&
+        _feedbackPinNumber != 2 &&
+        _feedbackPinNumber != 3 &&
+        _feedbackPinNumber != 7)
         exit(1);
-    #endif
+#endif
 }
 
-void FeedBackServo::feedback() {
-    if(digitalRead(feedbackPinNumber)) {
+void FeedBackServo::feedback()
+{
+    if (digitalRead(feedbackPinNumber))
+    {
         rise = micros();
         tLow = rise - fall;
 
         int tCycle = tHigh + tLow;
-        if((tCycle < 1000) || (tCycle > 1200))
+        if ((tCycle < 1000) || (tCycle > 1200))
             return;
-        
+
         float dc = (dutyScale * tHigh) / (float)tCycle;
         float theta = ((dc - dcMin) * unitsFC) / (dcMax - dcMin);
 
-        if(theta < 0.0)
+        if (theta < 0.0)
             theta = 0.0;
-        else if(theta > (unitsFC - 1.0))
+        else if (theta > (unitsFC - 1.0))
             theta = unitsFC - 1.0;
 
-        if((theta < q2min) && (thetaPre > q3max))
+        if ((theta < q2min) && (thetaPre > q3max))
             turns++;
-        else if((thetaPre < q2min) && (theta > q3max))
+        else if ((thetaPre < q2min) && (theta > q3max))
             turns--;
 
-        if(turns >= 0)
+        if (turns >= 0)
             angle = (turns * unitsFC) + theta;
-        else if(turns < 0)
+        else if (turns < 0)
             angle = ((turns + 1) * unitsFC) - (unitsFC - theta);
 
         thetaPre = theta;
-    } else {
+    }
+    else
+    {
         fall = micros();
         tHigh = fall - rise;
     }
--- src/FeedBackServo.h
+++ src/FeedBackServo.h
@@ -3,33 +3,34 @@
 #include <Arduino.h>
 #include <Servo.h>
 
-class FeedBackServo {
-    public:
-        FeedBackServo(byte feedbackPinNumber = 2);
-        void setServoControl(byte servoPinNumber = 3);
-        void setKp(float _Kp = 1.0);
-        void rotate(int degree, int threshold = 4);
-        int Angle();
-    
-    private:
-        void pinCheck(byte pinNumber);
-        void static feedback();
-        static Servo Parallax;
-        static byte feedbackPinNumber;
-        static volatile int angle;
-        static float thetaPre;
-        static unsigned int tHigh, tLow;
-        static unsigned long rise, fall;
-        static int turns;
-        static float Kp;
-        static const int unitsFC = 360;
-        static const float dcMin = 0.029;
-        static const float dcMax = 0.971;
-        static const int dutyScale = 1;
-        static const int q2min = unitsFC / 4;
-        static const int q3max = q2min * 3;
-        static const int degree = 0;
-        static const int threshold = 4;
+class FeedBackServo
+{
+public:
+    FeedBackServo(byte feedbackPinNumber = 2);
+    void setServoControl(byte servoPinNumber = 3);
+    void setKp(float _Kp = 1.0);
+    void rotate(int degree, int threshold = 4);
+    int Angle();
+
+private:
+    void pinCheck(byte pinNumber);
+    void static feedback();
+    static Servo Parallax;
+    static byte feedbackPinNumber;
+    static volatile int angle;
+    static float thetaPre;
+    static unsigned int tHigh, tLow;
+    static unsigned long rise, fall;
+    static int turns;
+    static float Kp;
+    static const int unitsFC = 360;
+    static const float dcMin = 0.029;
+    static const float dcMax = 0.971;
+    static const int dutyScale = 1;
+    static const int q2min = unitsFC / 4;
+    static const int q3max = q2min * 3;
+    static const int degree = 0;
+    static const int threshold = 4;
 };
 
 #endif
